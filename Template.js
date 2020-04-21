352b75ce691815bf485f08534f62e5c0ed44a0df
Mbulaheni Davhana <mbulaheni.d@gmail.com>
2020-04-21 13:57:18 +0200

    Fixed typo on the styles file

--- Template.js
+++ Template.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import styles from './Template.styles';
 
 const Template = props => {
     return (
-        <View style={styles.app}>
-
+        <View style={styles.screen}>
+            <Text>
+                The Template Component
+            </Text>
         </View>
     );
 };
--- Template.styles.js
+++ Template.styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justityContent: 'center',
+        justifyContent: 'center',
         alignItems: 'center',
     },
 });
